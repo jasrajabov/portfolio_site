e82fd7614234ade65329b3ce29c250e23d461c47
Jasurbek Rajabov <jas.rajabov@gmail.com>
2020-08-24 23:44:37 -0400

    fixed minor stuff

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER':'postgres',
-        'PASSWORD':'1Dalg091!',
+        'PASSWORD':'Postgres123!',
         'HOST':'localhost',
         'PORT':'5432',
     }
